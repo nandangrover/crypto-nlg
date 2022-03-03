daf47605727e886324fe5e6a46f5c7188eb597ec
Nandan Grover <nandangrover@Nandans-MacBook-Pro.local>
2022-03-03 13:39:36 +0000

    Added configurable coin json and parrot paraphraser

--- routes/api/nlg.js
+++ routes/api/nlg.js
@@ -6,9 +6,12 @@ const model_init = require("../../utils/model_initiator");
 // const { stripHtml } = require("string-strip-html");
 // const btc = require("../../config/btc.json");
 
-router.get("/", (req, res) => {
+router.get("/:coin", (req, res) => {
   // const fs = require('fs');
-  const btc = JSON.parse(fs.readFileSync("config/btc.json", "utf8"));
+
+  const coin_data = JSON.parse(fs.readFileSync(`config/${req.params.coin}.json`, "utf8"));
+  let main = JSON.parse(fs.readFileSync("config/main.json", "utf8"));
+  main.data[0].jsonData = coin_data;
 
   const options = {
     method: "POST",
@@ -18,7 +21,7 @@ router.get("/", (req, res) => {
       Authorization:
         "Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJhbF90ekdLVTFDQndMaHNPdk9qQmRBd1EiLCJpYXQiOjE2NDYwNTA4ODcsImV4cCI6MTgwMzczMDg4NywiaXNzIjoiQUxpdGUiLCJzdWIiOiJ4MEFjNXZWMXh0X3QiLCJBTGl0ZS5wZXJtIjpbInByczp4OmJ3anBMcW9ka0x2Il0sIkFMaXRlLnR0IjoidV9hIn0.DPICDsWnGGC53ypXn30_2NKIjyFunyjy4XKdNpRUWRD24FLhcTkMi1EOxzfBCIzexQCIasMhFtPqMA2zSaqiQQ",
     },
-    data: btc,
+    data: main,
   };
 
     axios
@@ -26,21 +29,18 @@ router.get("/", (req, res) => {
     .then(function (response) {
       const { result } = response.data[0];
 
-      const options2 = {
-        method: "POST",
-        url: "https://api.nlpcloud.io/v1/gpu/gpt-j/generation",
-        headers: {
-          "content-type": "application/json",
-          "Authorization":
-            "Token f701781ebfa97612580d11eae11ac9c2157e2db9"
-        },
-        data: {
-          max_length: 512,
-          text: result,
-        },
-      };
+      // const options2 = {
+      //   method: 'POST',
+      //   url: 'https://rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com/rewrite',
+      //   headers: {
+      //     'content-type': 'application/json',
+      //     'x-rapidapi-host': 'rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com',
+      //     'x-rapidapi-key': '1586fbc12bmsh35e444ee1dbc172p1f57bdjsne997fa52754e'
+      //   },
+      //   data: {language: 'en', strength: 3, text: result}
+      // };
 
-      // return axios.request(options2)
+      // return axios.request(options2);
       model_init(result, res);
     // res.json(response.data);
     })
