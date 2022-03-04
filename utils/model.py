33d81eff11c7b3586be06679cf55a69027c1de15
Stefan Rodrigues <stefanrodrigues@Stefans-MacBook-Air.local>
2022-03-04 18:33:21 +0000

    model file updated with fix

--- utils/model.py
+++ utils/model.py
@@ -1,33 +1,40 @@
-# from parrot import Parrot
-# import torch
-# import warnings
-# warnings.filterwarnings("ignore")
+from parrot import Parrot
+import torch
+import warnings
+import re
+warnings.filterwarnings("ignore")
 
-# # uncomment to get reproducable paraphrase generations
-# # def random_state(seed):
-# #   torch.manual_seed(seed)
-# #   if torch.cuda.is_available():
-# #     torch.cuda.manual_seed_all(seed)
+# uncomment to get reproducable paraphrase generations
+# def random_state(seed):
+#   torch.manual_seed(seed)
+#   if torch.cuda.is_available():
+#     torch.cuda.manual_seed_all(seed)
 
-# # random_state(1234)
+# random_state(1234)
+sys.argv[1]
+#Init models (make sure you init ONLY once if you integrate this to your code)
+parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5")
+coin_summary = "The live XRP price today is 0.81 USD with a 24-hour trading volume of 2,270,743,870.08 US Dollars. The XRP price had a loss of -0.76% in the last 24 hours. But it only went down -2.14% over the week. The Market cap of the XRP is 38,736,473,905 USD with a maximum supply of 100 billion XRP coins."
+coin_summary = coin_summary.replace("%","per").replace(" -"," ").split(". ")
+phrases = coin_summary
 
-# #Init models (make sure you init ONLY once if you integrate this to your code)
-# parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=True)
+out_data = ""
+for phrase in phrases:
+  print("-"*100)
+  print("Input_phrase: ", phrase)
+  print("-"*100)
+  para_phrases = parrot.augment(input_phrase=phrase,use_gpu=False,diversity_ranker="levenshtein",do_diverse=True)
+  try:
+    for para_phrase in para_phrases:
+      out_data += para_phrase[0].replace("per","%") + ". "
+      break
+  except:
+    phrase = phrase.replace(".","dot")
+    para_phrases = parrot.augment(input_phrase=phrase,use_gpu=False,diversity_ranker="levenshtein",do_diverse=True)
+    for para_phrase in para_phrases:
+      out_data += para_phrase[0].replace("per","%").replace("dot",".") + ". "
+      break
 
-# phrases = ["The live ADA price today is 0.88 USD with a 24-hour trading volume of 1,302,118,339.57 USD. The Cardano price went down by -1.21% in the last 24 hours. It gained 2.97% in the last 7 days. The Market cap of the Cardano is 28,354,543,219 USD with a maximum supply of 45 billion ADA coins.",
-#            "What are the famous places we should not miss in Russia?"
-# ]
+out_data = re.sub('\ 0', ' 0.', out_data)
 
-# for phrase in phrases:
-#   print("-"*100)
-#   print("Input_phrase: ", phrase)
-#   print("-"*100)
-#   para_phrases = parrot.augment(input_phrase=phrase, use_gpu=False,
-#                                diversity_ranker="levenshtein",
-#                                do_diverse=True, 
-#                                max_return_phrases = 10, 
-#                                max_length=512, 
-#                                adequacy_threshold = 0.99, 
-#                                fluency_threshold = 0.90)
-#   for para_phrase in para_phrases:
-print(1+1)
+print(out_data)
