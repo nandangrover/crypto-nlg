25f960f646ac2c0e254468aaa6a8c2a88fd06761
Nandan Grover <nandangrover@Nandans-MacBook-Pro.local>
2022-03-04 02:07:08 +0000

    modify server

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ app.use("/api/nlg", nlg);
 
 
 //Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 }
 const port = process.env.PORT || 8080;
 
